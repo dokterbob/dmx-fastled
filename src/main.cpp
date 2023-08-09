d2478175e568f5010c7f0e97c47668fd7b859f78
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2023-08-09 23:15:42 +0100

    YES!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <FastLED.h>
+#include <Candle.h>
 
 #define ONBOARD_LED 2
 
 #define NUM_LEDS    3
 
 CRGB leds[NUM_LEDS];
+candle::Candle candles[NUM_LEDS];
+candle::Candle saturation[NUM_LEDS];
 
 void setup() {
     delay( 3000 ); // power-up safety delay
@@ -12,8 +15,21 @@ void setup() {
     pinMode(ONBOARD_LED, OUTPUT);
 
     FastLED.addLeds<ESPDMX>(leds, NUM_LEDS);
-    FastLED.setCorrection(Typical8mmPixel);
+    FastLED.setCorrection(TypicalPixelString);
     FastLED.setTemperature(Candle);
+    // FastLED.setBrightness(8); // THIS WORKS SO FUCKING WELL!!!
+
+    for (size_t i=0; i<NUM_LEDS; i++) {
+        // Reset LED values
+        leds[i] = CRGB::Black;
+
+        // Initialize candles
+        candles[i].init(random8(16, 96), 255, random8(3, 8), random8(6, 22));
+
+        // Initialize random saturation
+        saturation[i].init(random8(112, 240), random8(241, 255), random8(10, 20), random8(22, 62));
+
+    }
 }
 
 
@@ -22,35 +38,17 @@ void loop()
 {
     digitalWrite(ONBOARD_LED,HIGH);
 
+    EVERY_N_MILLIS(20) {
+      for (size_t i=0; i<NUM_LEDS; i++) {
+          leds[i] = CHSV(hue + i*120, saturation[i].get_next_brightness(), candles[i].get_next_brightness());
+      }
+    }
+
     fill_rainbow(leds, NUM_LEDS, hue, 360/NUM_LEDS);
 
     FastLED.show();
-    FastLED.delay(100);
 
-    hue++;
+    EVERY_N_MILLIS(150) {
+      hue++;
+    }
 }
-
-
-
-
-// Additional notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
